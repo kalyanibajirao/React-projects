388999b0d49d0b035b5b07275604f233b5e7c2ca
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2023-08-03 14:27:45 +0530

    Add files via upload

---
+++ React Info Site/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from './Component/Main';
+import Navbar from './Component/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
