9fe5c9cb1a0bdf6dcf1cee19ec17d5fc69c4fb7d
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2023-08-03 14:20:42 +0530

    Add files via upload

---
+++ meme-generator-project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Component/Header';
+import Meme from './Component/Meme';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Meme/>
+    </div>
+  );
+}
+
+export default App;
